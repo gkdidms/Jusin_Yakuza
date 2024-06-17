51990fd3b74c48989ca8b2dfafc97ae724121dd9
gkdidms <gkdidms3320@gmail.com>
2024-06-17 12:21:41 +0900

    gitignore 수정

---
+++ Client/Bin/ShaderFiles/Shader_Deferred.hlsl
@@ -0,0 +1,455 @@
+
+#include "Engine_Shader_Defines.hlsli"
+
+/* 컨스턴트 테이블(상수테이블) */
+matrix g_WorldMatrix, g_ViewMatrix, g_ProjMatrix;
+matrix g_ViewMatrixInv, g_ProjMatrixInv;
+matrix g_LightViewMatrix, g_LightProjMatrix;
+
+vector g_vLightDir;
+vector g_vLightPos;
+float g_fLightRange;
+vector g_vLightDiffuse;
+vector g_vLightAmbient;
+vector g_vLightSpecular;
+
+vector g_vMtrlAmbient = vector(1.f, 1.f, 1.f, 1.f);
+vector g_vMtrlSpecular = vector(1.f, 1.f, 1.f, 1.f);
+
+vector g_vCamPosition;
+vector g_vCamDir;
+float g_fFar;
+
+Texture2D g_Texture;
+Texture2D g_NormalTexture;
+Texture2D g_DiffuseTexture;
+Texture2D g_ShadeTexture;
+Texture2D g_DepthTexture;
+Texture2D g_OutlineTexture;
+Texture2D g_GlowTexture;
+Texture2D g_GuassianTexture;
+Texture2D g_BackBufferTexture;
+Texture2D g_FinalBufferTexture;
+Texture2D g_LightDepthTexture;
+
+float g_fOutlineAngle = 0.8f;
+
+struct VS_IN
+{
+    float3 vPosition : POSITION;
+    float2 vTexcoord : TEXCOORD0;
+};
+
+struct VS_OUT
+{
+    float4 vPosition : SV_POSITION;
+    float2 vTexcoord : TEXCOORD0;
+};
+
+/* 정점 셰이더 :  /*
+/* 1. 정점의 위치 변환(월드, 뷰, 투영).*/
+/* 2. 정점의 구성정보를 변경한다. */
+VS_OUT VS_MAIN(VS_IN In)
+{
+    VS_OUT Out = (VS_OUT) 0;
+
+    matrix matWV, matWVP;
+
+    matWV = mul(g_WorldMatrix, g_ViewMatrix);
+    matWVP = mul(matWV, g_ProjMatrix);
+
+    Out.vPosition = mul(float4(In.vPosition, 1.f), matWVP);
+    Out.vTexcoord = In.vTexcoord;
+
+    return Out;
+}
+
+/* TriangleList인 경우 : 정점 세개를 받아서 w나누기를 각 정점에 대해서 수행한다. */
+/* 뷰포트(윈도우좌표로) 변환. */
+/* 래스터라이즈 : 정점으로 둘러쌓여진 픽셀의 정보를, 정점을 선형보간하여 만든다. -> 픽셀이 만들어졌다!!!!!!!!!!!! */
+struct PS_IN
+{
+    float4 vPosition : SV_POSITION;
+    float2 vTexcoord : TEXCOORD0;
+};
+
+struct PS_OUT
+{
+    vector vColor : SV_TARGET0;
+};
+
+PS_OUT PS_MAIN_DEBUG(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+
+    Out.vColor = g_Texture.Sample(LinearSampler, In.vTexcoord);
+
+    return Out;
+}
+
+struct PS_OUT_LIGHT
+{
+    vector vShade : SV_TARGET0;
+    vector vOutline : SV_TARGET1;
+};
+
+PS_OUT_LIGHT PS_MAIN_LIGHT_DIRECTIONAL(PS_IN In)
+{
+    PS_OUT_LIGHT Out = (PS_OUT_LIGHT) 0;
+    
+    vector vNormalDesc = g_NormalTexture.Sample(LinearSampler, In.vTexcoord);
+    vector vNormal = vector(vNormalDesc.xyz * 2.f - 1.f, 0.0f);
+    vector vDepthDesc = g_DepthTexture.Sample(PointSampler, In.vTexcoord);
+    vector vWorldPos;
+    
+    float fDot = max(dot(normalize(g_vLightDir) * -1.f, normalize(vNormal)), 0.f);
+    fDot = ceil(fDot * 3) / 3.f;
+    
+    Out.vShade = g_vLightDiffuse + saturate(fDot + (g_vLightAmbient * g_vMtrlAmbient * fDot));
+    
+    if (vDepthDesc.z == 1.f)
+    {
+        vWorldPos.x = In.vTexcoord.x * 2.f - 1.f;
+        vWorldPos.y = In.vTexcoord.y * -2.f + 1.f;
+        vWorldPos.z = vDepthDesc.x; /* 0 ~ 1 */
+        vWorldPos.w = 1.f;
+
+        vWorldPos = vWorldPos * (vDepthDesc.y * g_fFar);
+
+	/* 뷰스페이스 상의 위치를 구한다. */
+        vWorldPos = mul(vWorldPos, g_ProjMatrixInv);
+
+	/* 월드스페이스 상의 위치를 구한다. */
+        vWorldPos = mul(vWorldPos, g_ViewMatrixInv);
+    
+    //주변의 노말 벡터를 비교한다
+    
+        float fOffSetX = 1 / 1280.f;
+        float fOffSetY = 1 / 720.f;
+        float2 offsets[] =
+        {
+            float2(fOffSetX, 0.0f),
+            float2(-fOffSetX, 0.0f),
+            float2(0.0f, fOffSetY),
+            float2(0.0f, -fOffSetY)
+        };
+    
+    //위
+        float4 vNormalUpDesc = g_NormalTexture.Sample(LinearSampler, In.vTexcoord + offsets[2]);
+        vector vNormalUp = vector(vNormalUpDesc.xyz * 2.f - 1.f, 0.0f);
+    
+    //아래
+        float4 vNormalDownDesc = g_NormalTexture.Sample(LinearSampler, In.vTexcoord + offsets[3]);
+        vector vNormalDown = vector(vNormalDownDesc.xyz * 2.f - 1.f, 0.0f);
+    
+    //왼쪽
+        float4 vNormalLeftDesc = g_NormalTexture.Sample(LinearSampler, In.vTexcoord + offsets[1]);
+        vector vNormalLeft = vector(vNormalLeftDesc.xyz * 2.f - 1.f, 0.0f);
+    //오른쪽
+        float4 vNormalRightDesc = g_NormalTexture.Sample(LinearSampler, In.vTexcoord + offsets[0]);
+        vector vNormalRight = vector(vNormalRightDesc.xyz * 2.f - 1.f, 0.0f);
+        
+        //각도 계산하기
+        float4 vAvgNormal = (vNormalUp + vNormalDown + vNormalLeft + vNormalRight) / 4.f;
+        float fAngle = acos(dot(vNormal, vAvgNormal));
+        
+        if (fAngle > g_fOutlineAngle)
+        {
+            Out.vOutline = vector(0.3f, 0.2f, 0.1f, 1.f);
+            Out.vShade = vector(0.f, 0.f, 0.f, 1.f);
+        }
+        /*
+        float fAngleUp = acos(dot(vNormal, vNormalUp));
+        float fAngleDown = acos(dot(vNormal, vNormalDown));
+        float fAngleLeft = acos(dot(vNormal, vNormalLeft));
+        float fAngleRight = acos(dot(vNormal, vNormalRight));
+    
+
+        /*if (fAngleUp > fOutlineAngle || fAngleDown > fOutlineAngle || fAngleLeft > fOutlineAngle || fAngleRight > fOutlineAngle)
+        {
+            Out.vOutline = vector(0.3f, 0.2f, 0.1f, 1.f);
+            
+            Out.vShade = vector(0.f, 0.f, 0.f, 1.f);
+        }*/
+        
+    }
+   
+    return Out;
+}
+
+PS_OUT_LIGHT PS_MAIN_LIGHT_POINT(PS_IN In)
+{
+    PS_OUT_LIGHT Out = (PS_OUT_LIGHT) 0;
+
+    vector vNormalDesc = g_NormalTexture.Sample(LinearSampler, In.vTexcoord);
+    vector vNormal = vector(vNormalDesc.xyz * 2.f - 1.f, 0.0f);
+    vector vDepthDesc = g_DepthTexture.Sample(PointSampler, In.vTexcoord);
+    vector vWorldPos;
+
+    vWorldPos.x = In.vTexcoord.x * 2.f - 1.f;
+    vWorldPos.y = In.vTexcoord.y * -2.f + 1.f;
+    vWorldPos.z = vDepthDesc.x; /* 0 ~ 1 */
+    vWorldPos.w = 1.f;
+
+    vWorldPos = vWorldPos * (vDepthDesc.y * g_fFar);
+
+	/* 뷰스페이스 상의 위치를 구한다. */
+    vWorldPos = mul(vWorldPos, g_ProjMatrixInv);
+
+	/* 월드스페이스 상의 위치를 구한다. */
+    vWorldPos = mul(vWorldPos, g_ViewMatrixInv);
+
+
+    vector vLightDir = vWorldPos - g_vLightPos;
+    float fDistance = length(vLightDir);
+
+    float fAtt = max((g_fLightRange - fDistance), 0.f) / g_fLightRange;
+
+    Out.vShade = g_vLightDiffuse *
+		saturate(max(dot(normalize(vLightDir) * -1.f, normalize(vNormal)), 0.f) + (g_vLightAmbient * g_vMtrlAmbient)) * fAtt;
+    
+    return Out;
+}
+
+PS_OUT PS_MAIN_COPY_BACKBUFFER_RESULT(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+
+    vector vDiffuse = g_DiffuseTexture.Sample(LinearSampler, In.vTexcoord);
+    if (0.0f == vDiffuse.a)
+        discard;
+
+    vector vShade = g_ShadeTexture.Sample(LinearSampler, In.vTexcoord);
+    
+    vector vOutline = g_OutlineTexture.Sample(LinearSampler, In.vTexcoord);
+    
+    Out.vColor = vDiffuse * vShade + vOutline;
+    
+    vector vDepthDesc = g_DepthTexture.Sample(PointSampler, In.vTexcoord);
+    vector vWorldPos;
+
+    vWorldPos.x = In.vTexcoord.x * 2.f - 1.f;
+    vWorldPos.y = In.vTexcoord.y * -2.f + 1.f;
+    vWorldPos.z = vDepthDesc.x; /* 0 ~ 1 */
+    vWorldPos.w = 1.f;
+
+    vWorldPos = vWorldPos * (vDepthDesc.y * 3000.f);
+
+	/* 뷰스페이스 상의 위치를 구한다. */
+    vWorldPos = mul(vWorldPos, g_ProjMatrixInv);
+
+	/* 월드스페이스 상의 위치를 구한다. */
+    vWorldPos = mul(vWorldPos, g_ViewMatrixInv);
+
+    vector vLightPos = mul(vWorldPos, g_LightViewMatrix);
+    vLightPos = mul(vLightPos, g_LightProjMatrix);
+
+    float2 vTexcoord;
+    vTexcoord.x = (vLightPos.x / vLightPos.w) * 0.5f + 0.5f;
+    vTexcoord.y = (vLightPos.y / vLightPos.w) * -0.5f + 0.5f;
+
+    vector vLightDepthDesc = g_LightDepthTexture.Sample(PointSampler, vTexcoord);
+
+    float fLightOldDepth = vLightDepthDesc.x * 1000.f;
+
+    if (fLightOldDepth + 0.1f < vLightPos.w)
+        Out.vColor = vector(Out.vColor.rgb * 0.5f, 1.f);
+
+    return Out;
+}
+
+PS_OUT PS_MAIN_DEFERRED_RESULT(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+
+    Out.vColor = g_BackBufferTexture.Sample(LinearSampler, In.vTexcoord);
+
+    return Out;
+}
+
+float weights[9] =
+{
+        0.004f, 0.009f, 0.012f,
+        0.021f, 0.027f, 0.021f,
+    0.012f, 0.009f, 0.004f
+};
+
+float blurRadius = 2.f; // 흐림 정도를 조절하는 값
+
+PS_OUT PS_MAIN_GUASSIAN(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+    
+    float fNormalization = (weights[0] + 2.0f * (weights[1] + weights[2] + weights[3] + weights[4]));
+    float4 vSum = float4(0.f, 0.f, 0.f, 0.f);
+    
+    float fOffSetX = 1 / 1280.f;
+    float fOffSetY = 1 / 720.f;
+
+    float2 offsets[9] =
+    {
+        { -fOffSetX, -fOffSetY },
+        { 0.f, -fOffSetY },
+        { fOffSetX, -fOffSetY },
+        { -fOffSetX, 0.0f },
+        { 0.0f, 0.f },
+        { fOffSetX, 0.0f },
+        { -fOffSetX, fOffSetY },
+        { 0.0f, fOffSetY },
+        { fOffSetX, fOffSetY }
+    };
+    
+  // 가우시안 커널을 사용하여 픽셀 값을 샘플링하고 가중치를 적용합니다.
+    for (int i = 0; i < 9; ++i)
+    {
+        float2 vOffset = offsets[i] * blurRadius;
+        float4 vGlow = g_GlowTexture.Sample(LinearSampler, In.vTexcoord + vOffset);
+        float fWeight = weights[i] / fNormalization;
+        vSum += vGlow * fWeight;
+    }
+    
+    Out.vColor = vSum;
+    Out.vColor.a = 1.f;
+    
+    return Out;
+}
+
+
+PS_OUT PS_MAIN_GUASSIAN_DEFERREND_RESULT(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+    
+    vector vGuassian = g_GuassianTexture.Sample(LinearSampler, In.vTexcoord);
+    vector vBackBuffer = g_BackBufferTexture.Sample(LinearSampler, In.vTexcoord);
+    //vector vGlow = g_GlowTexture.Sample(LinearSampler, In.vTexcoord);
+    
+    Out.vColor = vBackBuffer + vGuassian;
+    
+    return Out;
+}
+
+PS_OUT PS_MAIN_FINAL_RESULT(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+    
+   // vector vGuassian = g_GuassianTexture.Sample(LinearSampler, In.vTexcoord);
+   // vector vBackBuffer = g_BackBufferTexture.Sample(LinearSampler, In.vTexcoord);
+    vector vFinal = g_FinalBufferTexture.Sample(LinearSampler, In.vTexcoord);
+    vector vGlow = g_GlowTexture.Sample(LinearSampler, In.vTexcoord);
+    
+    Out.vColor = vFinal + vGlow;
+    
+    return Out;
+}
+
+technique11 DefaultTechnique
+{
+
+	/* 특정 렌더링을 수행할 때 적용해야할 셰이더 기법의 셋트들의 차이가 있다. */
+    pass DebugRender // 0
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_Default, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+
+		/* 어떤 셰이덜르 국동할지. 셰이더를 몇 버젼으로 컴파일할지. 진입점함수가 무엇이찌. */
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_DEBUG();
+    }
+
+    pass Light_Directional // 1
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Blend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+    
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_LIGHT_DIRECTIONAL();
+    }
+
+    pass Light_Point // 2
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Blend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_LIGHT_POINT();
+    }
+
+    pass Copy_BackBuffer // 3
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Blend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_COPY_BACKBUFFER_RESULT();
+    }
+
+    pass Deferred_Result // 4
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_DEFERRED_RESULT();
+    }
+
+    pass Guassian_Result // 5
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_GUASSIAN();
+    }
+
+    pass Guassian_Deferred_Result // 6
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_GUASSIAN_DEFERREND_RESULT();
+    }
+
+    pass Final_Result // 7
+    {
+        SetRasterizerState(RS_Default);
+        SetDepthStencilState(DSS_None_Test_None_Write, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        HullShader = NULL;
+        DomainShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN_FINAL_RESULT();
+    }
+}
+
