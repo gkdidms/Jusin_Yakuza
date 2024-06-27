a42b06f0364e7dca91653271a4e5f16340a1eda3
Hyun2gi <leehj000920@ewhain.net>
2024-06-27 14:46:38 +0900

    [현정] 클라 CINE 카메라 수정, LIGHT 파일 수정

--- Client/Private/FileTotalMgr.cpp
+++ Client/Private/FileTotalMgr.cpp
@@ -75,7 +75,8 @@ HRESULT CFileTotalMgr::Set_Lights_In_Client(int iLightLoadingNum)
     /* 모든 라이트 삭제 */
     m_pGameInstance->Delete_AllLights();
 
-    Import_Bin_Light_Data_OnClient(&m_LightTotalInform, iLightLoadingNum);
+    if(FAILED(Import_Bin_Light_Data_OnClient(&m_LightTotalInform, iLightLoadingNum)))
+        return E_FAIL;
 
     for (int i = 0; i < m_LightTotalInform.iNumLights; i++)
     {
@@ -93,11 +94,11 @@ void CFileTotalMgr::Load_Cinemachine(int iCineNum, int iStageLevel)
 {
     if (nullptr == m_pCinemachineCam)
     {
-        CCineCamera::CINE_CAMERA_DESC		cineDesc;
-        cineDesc.iFileNum = iCineNum;
-        m_pGameInstance->Add_GameObject(iStageLevel, TEXT("Prototype_GameObject_CCineCamera"), TEXT("Layer_Camera"), &cineDesc);
-        m_pCinemachineCam = dynamic_cast<CCineCamera*>(m_pGameInstance->Get_GameObject(iStageLevel, TEXT("Layer_Camera"), CAMERA::CAMERA_CINEMACHINE));
-        //m_pCinemachineCam = dynamic_cast<CCineCamera*>(CGameInstance::GetInstance()->Clone_Object(TEXT("Prototype_GameObject_CCineCamera"), &cineDesc));
+        m_pCinemachineCam = dynamic_cast<CCineCamera*>(m_pGameInstance->Get_GameObject(iStageLevel, TEXT("Layer_Camera"), 1));
+        Safe_AddRef(m_pCinemachineCam);
+        m_pCinemachineCam->Load_CamBin(iCineNum);
+        m_pCinemachineCam->Initialize_Camera_Class();
+        m_pCinemachineCam->Setting_Start_Cinemachine();
     }
     else
     {
