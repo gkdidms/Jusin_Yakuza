c561d8d92d63464cdcbf3a4a3db88d333039a244
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2024-07-05 20:38:31 +0900

    병합 완료

--- Client/Private/AI_RushYakuza.cpp
+++ Client/Private/AI_RushYakuza.cpp
@@ -135,39 +135,6 @@ void CAI_RushYakuza::Ready_Tree()
 	m_pRootNode = pRoot;
 }
 
-CBTNode::NODE_STATE CAI_RushYakuza::Check_Angry()
-{
-	// 분노상태 전환 분기
-	if (!m_isAngry)
-	{
-		_uint iRandom = m_pGameInstance->Get_Random(0, 100);
-
-		//if (iRandom < 30.f)
-		//{
-		//	// 분노 상태로 이동
-		//	return CBTNode::SUCCESS;
-		//}
-
-		return CBTNode::FAIL;
-	}
-
-	//화가 나있는 상태에서 애니메이션이 진행중인가?
-	if (*m_pState == CMonster::MONSTER_ANGRY_START && m_pAnimCom->Get_AnimFinished())
-		return CBTNode::FAIL;
-	else if (*m_pState == CMonster::MONSTER_ANGRY_START) return CBTNode::RUNNING;
-
-	//이미 화가 나 있다면 바로 공격 모션으로 이동
-	return CBTNode::FAIL;
-}
-
-CBTNode::NODE_STATE CAI_RushYakuza::Angry()
-{
-	m_isAngry = true;
-	*m_pState = CMonster::MONSTER_ANGRY_START;
-
-	return CBTNode::SUCCESS;
-}
-
 CBTNode::NODE_STATE CAI_RushYakuza::Check_Attack()
 {
 	if (m_isAttack == false)
@@ -287,99 +254,6 @@ CBTNode::NODE_STATE CAI_RushYakuza::ATK_CMD()
 	}
 
 	return CBTNode::FAIL;
-}
-
-CBTNode::NODE_STATE CAI_RushYakuza::ATK_Angry_Punch()
-{
-	if (m_iSkill == SKILL_ANGRY_CHOP && m_isAttack)
-	{
-		if (*m_pState == CMonster::MONSTER_ANGRY_CHOP && m_pAnimCom->Get_AnimFinished())
-		{
-			m_isAttack = false;
-
-			return CBTNode::SUCCESS;
-		}
-
-		return CBTNode::RUNNING;
-	}
-
-	if (m_iSkill == SKILL_ANGRY_CHOP)
-	{
-		*m_pState = CMonster::MONSTER_ANGRY_CHOP;
-		m_isAttack = true;
-
-		return CBTNode::SUCCESS;
-	}
-
-	return CBTNode::FAIL;
-}
-
-CBTNode::NODE_STATE CAI_RushYakuza::ATK_Angry_Kick()
-{
-	if (m_iSkill == SKILL_ANGRY_KICK && m_isAttack)
-	{
-		if (*m_pState == CMonster::MONSTER_ANGRY_KICK && m_pAnimCom->Get_AnimFinished())
-		{
-			m_isAttack = false;
-
-			return CBTNode::SUCCESS;
-		}
-
-		return CBTNode::RUNNING;
-	}
-
-	if (m_iSkill == SKILL_ANGRY_KICK)
-	{
-		*m_pState = CMonster::MONSTER_ANGRY_KICK;
-		m_isAttack = true;
-
-		return CBTNode::SUCCESS;
-	}
-
-	return CBTNode::FAIL;
-}
-
-CBTNode::NODE_STATE CAI_RushYakuza::Check_Shift()
-{
-	if (m_iSkill != SKILL_SHIFT)
-		return CBTNode::SUCCESS;
-
-	if (m_fShiftDuration <= m_fShiftTime)
-	{
-		m_iSkill = SKILL_END;
-		m_fShiftTime = 0.f;
-		return CBTNode::FAIL;
-	}
-		
-	return CBTNode::RUNNING;
-}
-
-CBTNode::NODE_STATE CAI_RushYakuza::Shift()
-{
-	m_fShiftDuration = m_pGameInstance->Get_Random(1.f, 3.f);
-
-	_uint iIndex = m_pGameInstance->Get_Random(0, 10); // 랜덤 이동방향
-	m_iSkill = SKILL_SHIFT;
-
-	if (iIndex == 0)
-		*m_pState = CMonster::MONSTER_SHIFT_F;
-	else if (iIndex == 1)
-		*m_pState = CMonster::MONSTER_SHIFT_B;
-	else if (iIndex == 2)
-		*m_pState = CMonster::MONSTER_SHIFT_L;
-	else if (iIndex == 3)
-		*m_pState = CMonster::MONSTER_SHIFT_R;
-	else
-		return CBTNode::FAIL;
-
-	return CBTNode::SUCCESS;
-}
-
-CBTNode::NODE_STATE CAI_RushYakuza::Idle()
-{
-	*m_pState = CMonster::MONSTER_IDLE;
-
-	return CBTNode::SUCCESS;
 }
 
 CAI_RushYakuza* CAI_RushYakuza::Create(void* pArg)
