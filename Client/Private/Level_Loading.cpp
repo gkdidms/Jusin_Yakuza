ef51f9a45d6e5fabe19d907b52fb87cac6165d38
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2024-07-15 11:22:46 +0900

    릴리즈 확인 후 릴리즈 모드 수정

--- Client/Private/Level_Loading.cpp
+++ Client/Private/Level_Loading.cpp
@@ -84,6 +84,9 @@ void CLevel_Loading::Tick(const _float& fTimeDelta)
 
 #ifdef _DEBUG
 	SetWindowText(g_hWnd, m_pLoader->Get_LoadingText());
+#else
+	// 로딩 화면 만들어지기 전까지 릴리즈 모드에서도 볼 수 있게 해야함
+	SetWindowText(g_hWnd, m_pLoader->Get_LoadingText());
 #endif
 }
 
