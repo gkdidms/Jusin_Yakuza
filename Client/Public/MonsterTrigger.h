1eb5cdea787c92254a86d116f132d4d0a73c0fce
Hyun2gi <leehj000920@ewhain.net>
2024-07-19 10:22:15 +0900

    [현정] 맵데이터

---
+++ Client/Public/MonsterTrigger.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Trigger.h"
+
+class CMonsterTrigger : public CTrigger
+{
+private:
+    CMonsterTrigger(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+    CMonsterTrigger(const CMonsterTrigger& rhs);
+    virtual ~CMonsterTrigger() = default;
+
+public:
+    virtual HRESULT Initialize_Prototype() override;
+    virtual HRESULT Initialize(void* pArg) override;
+    virtual void Priority_Tick(const _float& fTimeDelta) override;
+    virtual void Tick(const _float& fTimeDelta) override;
+    virtual void Late_Tick(const _float& fTimeDelta) override;
+    virtual HRESULT Render() override;
+
+public:
+    virtual bool				Move_Scene(int& iLevelNum);
+
+private:
+    virtual HRESULT Add_Components(void* pArg) override;
+
+public:
+    static CMonsterTrigger* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+    virtual CGameObject* Clone(void* pArg);
+    virtual void Free() override;
+};
+
