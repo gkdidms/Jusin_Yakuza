385fa396a564b00c014dfe5ad6778ec248db29ed
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2024-07-25 16:54:55 +0900

    Transform 수정 및 길찾기 수정

--- Client/Private/Adventure.cpp
+++ Client/Private/Adventure.cpp
@@ -49,7 +49,9 @@ void CAdventure::Tick(const _float& fTimeDelta)
 		_bool isPicking = false;
 		_vector vGoalPos = m_pGameInstance->Picking(&isPicking);
 		if (isPicking)
+		{
 			m_pAStartCom->Start_Root(m_pNavigationCom, vGoalPos);
+		}
 	}
 	
 	Move_AStart();
@@ -271,18 +273,29 @@ void CAdventure::Synchronize_Root(const _float& fTimeDelta)
 
 void CAdventure::Move_AStart()
 {
-	list<CCell*>& BestCells = m_pAStartCom->Get_BestList();
+	list<CCell*>& FunnelList = m_pAStartCom->Get_FunnelList();
 
-	if (BestCells.empty())
+	if (FunnelList.empty())
 		return;
-
-	_vector	vDir = BestCells.front()->Get_WayPoint() - m_pTransformCom->Get_State(CTransform::STATE_POSITION);
+	
+	//웨이포인트가 아닌 선분의 중점을 기준으로 다가간다.
+	_vector vCellCheckPoint = (FunnelList.front()->Get_Point(CCell::POINT_B) + FunnelList.front()->Get_Point(CCell::POINT_C)) * 0.5f;
+	_vector	vDir = vCellCheckPoint - m_pTransformCom->Get_State(CTransform::STATE_POSITION);
 	_float fDist = XMVectorGetX(XMVector3Length(vDir));
+
+	m_pTransformCom->LookAt_For_LandObject(vCellCheckPoint);
 	
-	m_pTransformCom->LookAt_For_LandObject(BestCells.front()->Get_WayPoint());
-	
-	if (1.f >= fDist)
-		BestCells.pop_front();
+	//거의 근접하게 다가갈경우 제거한다.
+	if (0.1f >= fDist)
+	{
+		Safe_Release(FunnelList.front());
+		FunnelList.pop_front();
+	}
+}
+
+void CAdventure::Check_Separation()
+{
+	//내 범위 안에 들어온 NPC를 보고 피한다. 
 }
 
 HRESULT CAdventure::Add_Components()
