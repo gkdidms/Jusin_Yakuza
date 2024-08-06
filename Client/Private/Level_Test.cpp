c8d76d4678c452e0d2411e014f0a8515b3cd2e7d
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2024-08-06 19:03:32 +0900

    셰이더 수정중

--- Client/Private/Level_Test.cpp
+++ Client/Private/Level_Test.cpp
@@ -57,7 +57,7 @@ HRESULT CLevel_Test::Initialize()
 
 	/* Å¬¶ó ÆÄ½Ì */
 	m_pFileTotalManager->Set_MapObj_In_Client(7, LEVEL_TEST);
-	m_pFileTotalManager->Set_Lights_In_Client(0);
+	m_pFileTotalManager->Set_Lights_In_Client(90);
 	m_pFileTotalManager->Set_Collider_In_Client(0, LEVEL_TEST);
 	m_pFileTotalManager->Set_Trigger_In_Client(99, LEVEL_TEST);
 	
