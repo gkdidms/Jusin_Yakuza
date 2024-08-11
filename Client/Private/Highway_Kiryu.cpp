499be81c38ea799348c9a5dd250d64effed4809d
Me <Me@DESKTOP-7RM0KPK>
2024-08-11 10:06:03 +0900

    헤더 포함 수정

--- Client/Private/Highway_Kiryu.cpp
+++ Client/Private/Highway_Kiryu.cpp
@@ -7,7 +7,6 @@
 
 #include "SocketModel.h"
 #include "Weapon_Gun_Cz75.h"
-#include "Gun_Cz75.h"
 #include "UIManager.h"
 
 CHighway_Kiryu::CHighway_Kiryu(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
