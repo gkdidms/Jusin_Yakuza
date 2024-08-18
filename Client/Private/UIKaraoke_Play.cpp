a943d682ab571bfc839f976edfd05fc77e39e671
Me <Me@DESKTOP-7RM0KPK>
2024-08-19 02:45:59 +0900

    가라오케 진행중 (버그픽스)

--- Client/Private/UIKaraoke_Play.cpp
+++ Client/Private/UIKaraoke_Play.cpp
@@ -100,6 +100,11 @@ HRESULT CUIKaraoke_Play::Tick(const _float& fTimeDelta)
     Change_Lyrics();
     CurrentBar_Control();
 
+    if (m_pGameInstance->GetKeyState(DIK_LCONTROL))
+    {
+        m_pGameInstance->Set_SoundPosition(TEXT("Bakamita.mp3"), SOUND_BGM, 86.028);
+    }
+
     // 컷신 실행시키기
     if (CUTSCENE_START_POSITION < m_fCurSoundTime)
     {
