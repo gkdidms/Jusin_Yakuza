88e8f44fec0f0f3bf86fda125398b2d71dee55f8
Hyun2gi <leehj000920@ewhain.net>
2024-08-22 14:29:50 +0900

    [예은] 카체이스 몬스터 수정 중

--- Client/Private/Level_Roadway.cpp
+++ Client/Private/Level_Roadway.cpp
@@ -36,8 +36,6 @@ HRESULT CLevel_Roadway::Initialize()
 	if (nullptr == m_pCarChaseManager)
 		return E_FAIL;
 
-	m_pUIManager->Open_Scene(TEXT("Carchase"));
-
     /* Å¬¶ó ÆÄ½Ì */
     m_pFileTotalManager->Set_MapObj_In_Client(STAGE_ROADWAY, LEVEL_CARCHASE);
     m_pFileTotalManager->Set_Lights_In_Client(90);
@@ -55,7 +53,13 @@ HRESULT CLevel_Roadway::Initialize()
 
 void CLevel_Roadway::Tick(const _float& fTimeDelta)
 {
-	if (!m_pUIManager->isOpen(TEXT("Title")))
+	if (m_pUIManager->isTitleEnd() && !m_isTitleEnd)
+	{
+		m_pUIManager->Open_Scene(TEXT("Carchase"));
+		m_isTitleEnd = true;
+	}
+	
+	if (m_isTitleEnd)
 		m_pCarChaseManager->Tick();
 
 	m_pFightManager->Tick(fTimeDelta);
