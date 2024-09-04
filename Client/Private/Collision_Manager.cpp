25c0d7995eada5ff13043b8428cfc5a4c02e6457
Me <Me@DESKTOP-7RM0KPK>
2024-09-04 16:26:51 +0900

    콜리전 매니저 수정

--- Client/Private/Collision_Manager.cpp
+++ Client/Private/Collision_Manager.cpp
@@ -417,9 +417,6 @@ void CCollision_Manager::Item_Attack_Collision()
                 // 데미지를 제외하고 나머지는 임의의값을 넣음 (몬스터쪽에서 안쓰는값들)
                 pEnemyHitCollider->Get_Parent()->Take_Damage(0, _float3(), 10.f, nullptr, false);
                 dynamic_cast<CMonster*>(pEnemyHitCollider->Get_Parent())->Set_isColl(true);
-
-                // 아이템 콜라이더의 부모를 어떻게 꺼내오지?
-
             }
         }
     }
