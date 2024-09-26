827dd3c1b0862cd88f6ad43f0fc09a03abecc0c7
Me <Me@DESKTOP-7RM0KPK>
2024-09-26 22:55:07 +0900

    촬영용

--- Client/Private/Level_Test.cpp
+++ Client/Private/Level_Test.cpp
@@ -222,7 +222,7 @@ HRESULT CLevel_Test::Ready_Test_Hyewon()
 	// 도지마조 로비 배경 테스트맵
 	// 99(빈맵) 98(Rush) 97(WPH) 96(ShakeDown) 95(WPA) 94(요네다) 89(디폴트)
 	// 도지마조 로비 네비: 5
-	m_pFileTotalManager->Set_MapObj_In_Client(99, LEVEL_TEST);
+	m_pFileTotalManager->Set_MapObj_In_Client(89, LEVEL_TEST);
 	m_pFileTotalManager->Set_Lights_In_Client(5);
 	m_pFileTotalManager->Set_Collider_In_Client(0, LEVEL_TEST);
 	//m_pFileTotalManager->Set_Trigger_In_Client(79, LEVEL_TEST);
